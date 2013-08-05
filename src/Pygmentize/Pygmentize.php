93e6d45f46da422d100de8971b439c0c9deff897
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-06 01:13:19 +0200

    first version

---
+++ src/Pygmentize/Pygmentize.php
@@ -0,0 +1,76 @@
+<?php
+
+//! @file Pygmentize.php
+//! @brief This file contains the Pygmentize class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+//! @brief Global namespace for the Pygmentize class.
+namespace Pygmentize;
+
+
+//! @brief Pygments is a wrapper to 'pygmentize', the command line interface provided by Pygments, a python syntax
+//! highlighter.
+class Pygmentize {
+
+  // Binary name.
+  const PIGMENTS_BINARY = "pygmentize";
+
+
+  //! @brief Formats the provided source code using the specified formatter and style.
+  //! @param[in] string $source The source code.
+  //! @param[in] string $language The programming language name of the source code.
+  //! @param[in] string $encoding The file input and output encodings.
+  //! @param[in] string $formatter The output will be created using the provided formatter.
+  //! @param[in] string $style Rhe style used by the formatter.
+  //! @return string The highlighted source code.
+  public static function highlight($source, $language, $encoding = "utf-8", $formatter = "html", $style = "borland") {
+
+    // Try to create a temporary physical file. The function 'proc_open' doesn't allow to use a memory file.
+    if ($fd = fopen("php://temp", "r+")) {
+      fputs($fd, $source); // We don't need to flush because we call rewind.
+      rewind($fd); // Sets the pointer to the beginning of the file stream.
+
+      $dspec = array(
+        $fd,
+        1 => array('pipe', 'w'), // stdout
+        2 => array('pipe', 'w'), // stderr
+      );
+
+      $args = sprintf(" -l %s -f %s -O encoding=%s,style=%s,lineos=1", $language, $formatter, $encoding, $style);
+      $proc = proc_open(self::PIGMENTS_BINARY.$args, $dspec, $pipes);
+
+      if (is_resource($proc)) {
+        // Reads the stdout output.
+        $output = "";
+        while (!feof($pipes[1])) {
+          $output .= fgets($pipes[1]);
+        }
+
+        // Reads the stderr output.
+        $error = "";
+        while (!feof($pipes[2])) {
+          $error .= fgets($pipes[2]);
+        }
+
+        // Free all resources.
+        fclose($fd);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        $exitCode = proc_close($proc);
+
+        if ($exitCode != 0)
+          throw new \RuntimeException($error);
+
+        return $output;
+      }
+      else
+        throw new \RuntimeException("Cannot execute the 'pygmentize' command.");
+    }
+    else
+      throw new \RuntimeException("Cannot create the temporary file with the source code.");
+
+  }
+
+}
